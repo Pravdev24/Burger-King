d8aadb9d05a71bb29b4964bc0b26cd6b2560e1a8
Praveen <sudharpraveen1995@gmail.com>
2022-05-24 18:55:23 +0530

    added order file

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// ....
+
+function newWindow() {
+  var input = document.getElementById("burgerName").value;
+  if (
+    input === "The Pickle Bacon" ||
+    input === "The Turkey & Egg" ||
+    input === "The All American"
+  ) {
+    window.location.href = "order.html";
+  } else {
+    alert("Enter Burger name");
+  }
+}
+
+document.getElementById("submit").addEventListener("click", (e) => {
+  e.preventDefault();
+  newWindow();
+});
+
+// year
+
 const year = new Date().getFullYear();
 
 document.getElementById("year").innerHTML =
